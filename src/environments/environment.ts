92ed5ae0bc4b4fe01612d3744aa683e0cdbada98
abeja01 <TU_CORREO_GITHUB>
2026-06-05 18:57:30 -0500

    actualizado

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCV3duf_77YL6x1x9NTCAN2Eg033TPUYdY",
+    authDomain: "inventario-6c92f.firebaseapp.com",
+    projectId: "inventario-6c92f",
+    storageBucket: "inventario-6c92f.firebasestorage.app",
+    messagingSenderId: "270951204436",
+    appId: "1:270951204436:web:fd127a1a1e797b6adc8fc1",
+    measurementId: "G-17GF2GEN26"
+  }
+};
